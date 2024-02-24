4328e36b4c98414625769927fe13ba2f5f9f311d
geno223 <eugenio.jimenez22397@gmail.com>
2024-02-24 11:08:09 -0500

    commit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-
-
+const morgan = require('morgan');
+const cors = require('cors')
+const EventRouter = require('./controller/event') //new
 
 const app = express();
 
+//middleware
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+//routes
+app.use('/event', EventRouter);
 app.get('/', (req, res) => (
     res.send('meow world')
 ))
